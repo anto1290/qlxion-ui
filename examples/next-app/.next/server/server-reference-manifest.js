69aa37aad2f768c28bb294ba8fa58a1bdea4d343
Muhammad Nurwibawanto <mnurwibawanto@gmail.com>
2026-09-21 10:16:31 +0700

    fix again

--- examples/next-app/.next/server/server-reference-manifest.js
+++ examples/next-app/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SYTlWpxR6y7elS0+H30HaHpWh0KRKDP/A0HNh8Loi8k=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"UonUSfd0zcZqcmm/moL438WWqI7kkSILLuVhJVmnu8w=\"}"
